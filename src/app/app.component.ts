87961f805027e691cf6261093a7797d8a7e61424
Lucas Rassilan Vilanova <lucasrsv1@gmail.com>
2025-02-25 20:38:34 -0300

    Contadores de palavras no menu lateral

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,8 @@
 import { Component } from "@angular/core";
-import { AsyncPipe, NgForOf, NgIf } from "@angular/common";
+import { AsyncPipe, NgIf } from "@angular/common";
 import { RouterLink, RouterLinkActive } from "@angular/router";
 
 import { addIcons } from "ionicons";
-import { RouterDirection } from "@ionic/core";
 import { bookOutline, checkmarkDoneOutline, closeCircleOutline, homeOutline, logOutOutline, searchOutline, textOutline } from "ionicons/icons";
 import { IonApp, IonAvatar, IonBadge, IonContent, IonIcon, IonItem, IonLabel, IonList, IonListHeader, IonMenu, IonMenuToggle, IonRouterLink, IonRouterOutlet, IonSplitPane, IonText } from "@ionic/angular/standalone";
 
@@ -15,15 +14,11 @@ import { RoundProgressModule } from "angular-svg-round-progressbar";
 import { IUser } from "./models/user";
 
 import { AuthenticationService } from "./services/authentication/authentication.service";
-
-interface AppLink {
-	title: string;
-	url: string;
-	icon: string;
-	direction: RouterDirection;
-	current: number;
-	max: number;
-}
+import { CommonWordsService } from "./services/training/common-words/common-words.service";
+import { DictionaryService } from "./services/dictionary/dictionary.service";
+import { KnownWordsService } from "./services/training/known-words/known-words.service";
+import { MisspelledWordsService } from "./services/training/misspelled-words/misspelled-words.service";
+import { WordsToReviewService } from "./services/training/words-to-review/words-to-review.service";
 
 @Component({
 	selector: "app-root",
@@ -32,7 +27,6 @@ interface AppLink {
 	imports: [
 		AsyncPipe,
 		NgIf,
-		NgForOf,
 		RouterLink,
 		RouterLinkActive,
 		IonApp,
@@ -54,15 +48,17 @@ interface AppLink {
 	]
 })
 export class AppComponent {
-	public readonly appPages: AppLink[] = [
-		{ title: "Common Words", url: "/train/common-words", icon: "text", direction: "forward", current: 13, max: 48 },
-		{ title: "All Words", url: "/train/all-words", icon: "book", direction: "forward", current: 24, max: 155 }
-	];
-
 	public user$: Observable<IUser | null>;
 	public isLoggedIn$: Observable<boolean>;
+	public commonCounter = { value: 0, percentage: 0 };
+	public knownCounter = { value: 0, percentage: 0 };
 
 	constructor (
+		public readonly dictionaryService: DictionaryService,
+		public readonly commonWordsService: CommonWordsService,
+		public readonly knownWordsService: KnownWordsService,
+		public readonly misspelledWordsService: MisspelledWordsService,
+		public readonly wordsToReviewService: WordsToReviewService,
 		private readonly authenticationService: AuthenticationService
 	) {
 		StatusBar.setBackgroundColor({ color: "#333333" });
@@ -70,6 +66,16 @@ export class AppComponent {
 
 		this.user$ = this.authenticationService.user$;
 		this.isLoggedIn$ = this.authenticationService.isLoggedIn$;
+
+		this.commonWordsService.commonCounter$.subscribe(counter => {
+			this.commonCounter.value = counter;
+			this.commonCounter.percentage = Math.round(counter / this.commonWordsService.words.size * 100);
+		});
+
+		this.knownWordsService.knownCounter$.subscribe(counter => {
+			this.knownCounter.value = counter;
+			this.knownCounter.percentage = Math.round(counter / this.dictionaryService.words.size * 100);
+		});
 	}
 
 	public get userInitials (): string {
